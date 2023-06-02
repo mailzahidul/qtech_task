67d8ef04ac61c9b1daecf33e3e0831d853ab1df3
Zahidul Islam <mailzahidul@gmail.com>
2023-06-03 01:05:13 +0600

    Update settings.py
    allow host assigned

--- qtech_task_project/settings.py
+++ qtech_task_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9y!0t3+aes4(fd5py-ru)2_sxnj8v)@rlm9u76els8dvqbm82c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
